ba16b50799d8dbe4fb0580a453f0aeec6f1074e2
Sil van Diepen <me@silvandiepen.com>
2019-01-09 14:18:11 +0100

    add grid separate

--- script/sass.js
+++ script/sass.js
@@ -2,21 +2,39 @@ var sass = require('sass');
 var Fiber = require('fibers');
 var fs = require('fs');
 
-
-sass.render(
-	{
-		file: 'src/scss/app.scss',
-		// importer: function(url, prev, done) {
-		//   // ...
-		// },
-		fiber: Fiber
-	},
-	function(err, result) {
-		if (err) throw err;
-		
-		fs.writeFile('dist/app.css', result.css, (err) => {
+function renderBaseStyle() {
+	sass.render(
+		{
+			file: 'src/scss/app.scss',
+			fiber: Fiber
+		},
+		function(err, result) {
 			if (err) throw err;
-			console.log('The file has been saved!');
-		});
-	}
-);
+
+			fs.writeFile('dist/app.css', result.css, (err) => {
+				if (err) throw err;
+				console.log('App has been saved!');
+			});
+		}
+	);
+}
+
+function renderGridStyle() {
+	sass.render(
+		{
+			file: 'src/scss/grid.scss',
+			fiber: Fiber
+		},
+		function(err, result) {
+			if (err) throw err;
+
+			fs.writeFile('dist/grid.css', result.css, (err) => {
+				if (err) throw err;
+				console.log('Grid has been saved!');
+			});
+		}
+	);
+}
+
+renderBaseStyle();
+renderGridStyle();
